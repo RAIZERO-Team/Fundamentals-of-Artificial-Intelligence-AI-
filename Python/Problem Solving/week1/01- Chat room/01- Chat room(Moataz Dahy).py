46569ff962f6c3f08ff390eb00201e23b4138c0f
Moataz <abdorashwan536@gmail.com>
2024-07-23 00:09:56 +0300

    update the first problem in week 1

--- Python/Problem Solving/week1/01- Chat room/01- Chat room(Moataz Dahy).py
+++ Python/Problem Solving/week1/01- Chat room/01- Chat room(Moataz Dahy).py
@@ -1,24 +1,15 @@
 '''Moataz Dahy'''
 
-X , Y = map(float,input().split())
 
-if X == 0 and Y == 0:
-  print("Origem")
+word = list("hello")
+s = input()
+count = 0
 
-elif X == 0:
-  print("Eixo Y")
-
-elif Y == 0:
-  print("Eixo X")
-
-elif X > 0 and Y > 0:
-  print("Q1")
-
-elif X < 0 and Y > 0:
-  print("Q2")
-
-elif X < 0 and Y < 0:
-  print("Q3")
-
-elif X > 0 and Y < 0:
-  print("Q4")
+for i in s:
+    if i == word[count]:
+        count += 1
+    if count == len(word):
+        print("YES")
+        break
+else:
+    print("NO")
