663081e3a7907bb0838146c16a682935f579c84c
Mohamed <mohammedelkady8889@gmail.com>
2024-08-11 07:18:49 +0300

    solev problem 3

---
+++ Python/Problem Solving/week2/03- Prime Fibonacci/03- Prime Fibonacci (mohamed khalifa).py
@@ -0,0 +1,5 @@
+# Author : mohamed khalifa
+
+prime = [0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]
+
+for _ in range(int(input())) : print("prime" if prime[int(input())] else "not prime")
